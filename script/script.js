e7903394c08c325fed1d1c9e24ba6a506bd909a9
GENSECvkpb <44395857+GENSECvkpb@users.noreply.github.com>
2020-03-05 16:40:14 +0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,8 @@
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+    $('html, body').animate({scrollTop: $(target).offset().top}, 800);//800 - длительность скроллинга в мс
+        return false;
+    });
+});
+    
